442040289b92381b27f261b6d9a2682da28fa36f
iped <iped@sas.upenn.edu>
2016-12-22 15:04:44 -0500

    updated FR3_config for sys3

--- FR3_config.py
+++ FR3_config.py
@@ -2,33 +2,28 @@
 # FOR FR IN SYSTEM 2.0
 
 # ALL SYSTEM2.0 CONFIGURATION OPTIONS
-sys2 = {\
-        'EXPERIMENT_NAME'  : 'FR3',
-        'STIM_TYPE'        : 'CLOSED_STIM',
-        'VERSION_NUM'      : '2.04',
-        'control_pc'       : 1,              # Will be incremented later for other control pc versions.  Set to 0 to turn off control pc processing
-        'heartbeat'        : 1000,            # milliseconds
-        'syncMeasure'      : False,         # If True, sync pulses are sent to the syncbox at the same time as a 'SYNC' messages to the Control PC
-        'syncCount'        : 5,              # number of sync messages before control PC is 'synced'
-        'syncInterval'     : 500,          # milliseconds
-        'is_hardwire'      : True,         # Should be set to 'True' in production code
-        'state_list'       : 
-                    [
-                        'PRACTICE',
-                        'STIM ENCODING',
-                        'NON-STIM ENCODING',
-                        'RETRIEVAL',
-                        'DISTRACT',
-                        'INSTRUCT',
-                        'COUNTDOWN',
-                        'WAITING',
-                        'WORD',
-                        'ORIENT',
-                        'MIC TEST',
-                     ]
-        }
+experiment = 'FR3'
+stim_type = 'CLOSED_STIM'
+version = '3.0'
+control_pc = True
+heartbeat_interval = 1000
+state_list = [ 
+    'PRACTICE',
+    'STIM ENCODING',
+    'NON-STIM ENCODING',
+    'RETRIEVAL',
+    'DISTRACT',
+    'INSTRUCT',
+    'COUNTDOWN',
+    'WAITING',
+    'WORD',
+    'ORIENT',
+    'MIC TEST',
+ ]
 
 require_labjack = False
+
+do_stim = True
 
 numSessions = 10
 
@@ -38,7 +33,3 @@ nControlTrials = 11
 
 # %s will be replaced by config.LANGUAGE
 wordList_dir = 'pools_%s/stim_lists'
-
-# Control PC options
-sessionType = 'CLOSED_STIM'
-
