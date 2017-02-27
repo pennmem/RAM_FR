878b0cf4e49b240838b36bfb0f405762f043350e
Michael V. DePalatis <mike@depalatis.net>
2017-02-27 17:49:25 -0500

    Add FR5 config file

---
+++ FR5_config.py
@@ -0,0 +1,12 @@
+version = "5.0.0-dev"
+
+numSessions = 10
+
+nStimTrials = 14
+nBaselineTrials = 3
+nControlTrials = 8
+
+# %s will be replaced by config.LANGUAGE
+wordList_dir = 'pools_%s/stim_lists'
+
+lure_list = "pools_%s/FR5_RecognitionTaskLures.txt"
